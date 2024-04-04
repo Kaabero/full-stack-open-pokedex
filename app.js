f4a845322754fd984bb6a559a4d5a830dbd53e36
Katri <katri.kuoppala@helsinki.fi>
2024-04-04 10:55:07 +0300

    testing error notification

--- app.js
+++ app.js
@@ -9,16 +9,11 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) => {
   res.send('10') // change this string to ensure a new version deployed
 })
-/*
+
 app.get('/health', (req, res) => {
   res.send('ok')
 })
-*/
-app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
-  res.send('ok')
-})
+
 
 app.listen(PORT, () => {
   /* eslint-disable no-console */
